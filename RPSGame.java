c9689755f383e43937d12efbe9e4351f24c53ceb
Hyperlolo <hyperlolorb@gmail.com>
2017-11-10 18:43:17 -0800

    Moved normal RPS to its own method inorder to have the game loop if the user doesn't want to end it, also made random rps method

--- RPSGame.java
+++ RPSGame.java
@@ -11,9 +11,8 @@ public class RPSGame {
 	public static void main(String[] args) { // Main, user chooses what kind of a game mode they want to play, a random
 												// mode or a straight forward mode
 		Scanner reader = new Scanner(System.in);
-		int userScore = 0, compScore = 0, counter = 0;
-		int round = 1, game = 1;
-		char userRPSChoice, compChoice, tieBreaker, userChoice;
+		int gameWinner = 3;
+		char userRPSChoice;
 		String winner = null;
 		System.out
 				.print("Welcome to this game of rock paper scissors where you can break your ties using other games.");
@@ -22,12 +21,26 @@ public class RPSGame {
 			userRPSChoice = reader.next().charAt(0);
 		} while (userRPSChoice != 'r' && userRPSChoice != 'R' && userRPSChoice != 'n' && userRPSChoice != 'N');
 		if (userRPSChoice == 'r') {// Takes to randRps method
-			System.out.println("-----------------------------------------------------------------------------------------------\nYou have chosen to play Rock Paper Scissors randomly");
 			randRps();
+			System.out.println(
+					"-----------------------------------------------------------------------------------------------\nYou have chosen to play Rock Paper Scissors randomly");
+			randRps();
+		} else {
+			normRps();
+
 		}
-		// If user chose to do a normal game then they get to pick their tie breaker
-		// game rather than have it be randomly picked for them.
-		System.out.println("-----------------------------------------------------------------------------------------------\nYou have chosen to play Rock Paper Scissors normally");
+	}// end main
+
+	public static int normRps() {// If user chose to do a normal game then they get to pick their tie breaker
+									// game rather than have it be randomly picked for them.
+		Scanner reader = new Scanner(System.in);
+		int userScore = 0, compScore = 0, counter = 0;
+		int gameWinner = 3;
+		int round = 1, game = 1;
+		char compChoice, tieBreaker, userChoice;
+		String winner = null;
+		System.out.println(
+				"-----------------------------------------------------------------------------------------------\nYou have chosen to play Rock Paper Scissors normally");
 		do {// Start of normal RPS
 			String compChoices = "rps"; // The avaliable string choices that the computer can pick from
 			int compRand = (int) (Math.random() * 3); // Comp picks 1-3 numbers randomly
@@ -46,6 +59,7 @@ public class RPSGame {
 					System.out.print("\n" + round + "\t\t\t" + compChoice + "  \t\t\t" + userChoice + "\t\t   " + winner
 							+ "                     " + userScore + "                       " + compScore);
 					round++;
+
 				} else if (userChoice == 'r' && compChoice == 's' || userChoice == 's' && compChoice == 'p'
 						|| userChoice == 'p' && compChoice == 'r') {// User wins
 					userScore++;
@@ -64,19 +78,73 @@ public class RPSGame {
 			}
 			if (userScore >= 2) {
 				System.out.println("\n\nYou won this game, please restart to play another game.");
-				break;
+				gameWinner = 1;
 			}
 			if (compScore >= 2) {
 				System.out.println("\n\nThe computer won this game, please restart to try and beat it.");
+				gameWinner = 0;
 			}
 			counter++;
 			game = 1;
 		} while (counter <= 10);
-
-	}// end main
+		return gameWinner;
+	}
 
 	public static int randRps() { // Random rock paper Scissors Game
+		Scanner reader = new Scanner(System.in);
+		int userScore = 0, compScore = 0, counter = 0;
+		int gameWinner = 3;
+		int round = 1, game = 1;
+		char compChoice, tieBreaker, userChoice;
+		String winner = null;
+		System.out.println(
+				"-----------------------------------------------------------------------------------------------\nYou have chosen to play Rock Paper Scissors randomly");
+		do {// Start of normal RPS
+			String compChoices = "rps"; // The avaliable string choices that the computer can pick from
+			int compRand = (int) (Math.random() * 3); // Comp picks 1-3 numbers randomly
+			compChoice = compChoices.charAt(compRand); // 1 number picked above, then assigned to the corresponding
+														// string
+			do {
+				System.out.println("\nPlease pick Rock(R), Paper(P), or Scissors(S)");
+				userChoice = reader.next().charAt(0);// Asks user for their choice
+			} while (userChoice != 'r' && userChoice != 'R' && userChoice != 'p' && userChoice != 'P'
+					&& userChoice != 's' && userChoice != 'S');// User can only exit if they pick one of these
+			System.out.print("Round\t\tComputer Choice\t\t  User Choice\t\t  Winner\t\tUser Score\t\tComp Score");
+			for (game = 1; game <= 1; game++) { // Runs it once and then reset for next round
+				if (userChoice == compChoice) { // Tie, user can choose to break the tie with a specific game or
+												// continue without choosing
+					winner = "Tie";
+					System.out.print("\n" + round + "\t\t\t" + compChoice + "  \t\t\t" + userChoice + "\t\t   " + winner
+							+ "                     " + userScore + "                       " + compScore);
+					round++;
 
-		return 1;
+				} else if (userChoice == 'r' && compChoice == 's' || userChoice == 's' && compChoice == 'p'
+						|| userChoice == 'p' && compChoice == 'r') {// User wins
+					userScore++;
+					winner = "User";
+					System.out.print("\n" + round + "\t\t\t" + compChoice + "  \t\t\t" + userChoice + "\t\t   " + winner
+							+ "                     " + userScore + "                       " + compScore);
+
+					round++;
+				} else {// Comp wins
+					compScore++;
+					winner = "Computer";
+					System.out.print("\n" + round + "\t\t\t" + compChoice + "  \t\t\t" + userChoice + "\t\t " + winner
+							+ "                   " + userScore + "                       " + compScore);
+					round++;
+				}
+			}
+			if (userScore >= 2) {
+				System.out.println("\n\nYou won this game, please restart to play another game.");
+				gameWinner = 1;
+			}
+			if (compScore >= 2) {
+				System.out.println("\n\nThe computer won this game, please restart to try and beat it.");
+				gameWinner = 0;
+			}
+			counter++;
+			game = 1;
+		} while (counter <= 10);
+		return gameWinner;
 	}// end rPS
 }// end class
